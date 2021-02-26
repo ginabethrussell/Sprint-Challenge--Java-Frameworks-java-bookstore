853242ce0c4ffea2d4de46fbaff3a81361bb3720
ginabethrussell <ginabeth.russell@gmail.com>
2021-02-26 08:09:48 -0500

    added antmatcher for book routes to allow all authenticated users, added further restriction in the bookcontroller to only allow admin to post, put, and delete

--- src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
+++ src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
@@ -54,6 +54,7 @@ public class ResourceServerConfig
                 .permitAll()
                 .antMatchers("/users/**",
                              "/useremails/**",
+                             "/books/**",
                              "/oauth/revoke-token",
                              "/logout")
                 .authenticated()
